e210d37a0a7d4603bd9ff540245fe0acaf45f637
Ross McLean <ross_mclean@posteo.net>
2020-12-02 05:05:20 +0000

    Get university JSON data
    Signed-off-by: Ross McLean <ross_mclean@posteo.net>

--- scripts/js/generate_uni_list.js
+++ scripts/js/generate_uni_list.js
@@ -1,94 +1,37 @@
-let xmlhttp = new XMLHttpRequest();
-xmlhttp.onreadystatechange = function () {
+/*
+file: generate_uni_list.js
+author: Ross McLean
+desc: Gets universities from an API and builds a list from the results
+ */
+
+const container = document.getElementById("uni-link-list");
+const textbox = document.getElementById("uni");
+let baseUrl = "http://universities.hipolabs.com/search?country=United Kingdom";
+
+let requestUrl = textbox.value ? `${baseUrl}${textbox.value}` : baseUrl;
+
+container.setAttribute("class", "container");
+let request = new XMLHttpRequest();
+
+request.open("GET", requestUrl, true);
+console.log('OPENED', request.readyState);
+request.onreadystatechange = function () {
     // https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest/readyState
     if (this.readyState === 4 && this.status === 200) {
-        const json = JSON.parse(xmlhttp.responseText);
+        const data = JSON.parse(request.response);
 
+        data.forEach(uni => {
+            const div = document.createElement("div");
+            const h3 = document.createElement("h3");
+            const p = document.createElement("p");
 
-        // Increment counters for chart generation
-        for (let entry in json) {
-            if (json.hasOwnProperty(entry)) {
-                // Get keys/columns
-                transport = json[entry].transport;
+            h3.textContent = uni["name"];
+            p.textContent = uni["web_pages"][0];
 
-            }
-
-            const values = ["train", "taxi", "bus", "hotel ", "hostel ", "camping ", "pool", "voucher", "safe"];
-            for (let value in values) {
-
-                // Define table rows
-                const node = document.createElement("tr");
-                const subNode1 = document.createElement("td");
-                const textNode1 = document.createTextNode(values[value]);
-                const subNode2 = document.createElement("td");
-                const subSubNode = document.createElement("canvas");
-
-                subNode1.appendChild(textNode1);
-                subNode2.appendChild(subSubNode);
-
-                // Set canvas attributes
-                const att1 = subSubNode.setAttribute("id", values[value]);
-                const att2 = subSubNode.setAttribute("width", "800");
-                const att3 = subSubNode.setAttribute("height", "25");
-
-                // Nest child nodes in parent node
-                node.appendChild(subNode1);
-                node.appendChild(subNode2);
-
-                // Nest all nodes in parent table element
-                document.getElementById("chart").appendChild(node);
-
-                // Create bars
-                const canvas = document.getElementById(values[value]);
-                const bar = canvas.getContext("2d");
-
-                let length = 0;
-                switch (values[value]) {
-                    case "train":
-                        length = trainCount;
-                        break;
-                    case "taxi":
-                        length = taxiCount;
-                        break;
-                    case "bus":
-                        length = busCount;
-                        break;
-                    case "hotel ":
-                        length = hotelCount;
-                        break;
-                    case "hostel ":
-                        length = hostelCount;
-                        break;
-                    case "camping ":
-                        length = campingCount;
-                        break;
-                    case "pool":
-                        length = poolCount;
-                        break;
-                    case "voucher":
-                        length = voucherCount;
-                        break;
-                    case "safe":
-                        length = safeCount;
-                        break;
-                    default:
-                        console.log(`Error: is value the correct string? - ${values[value]}`);
-                }
-
-                if (length > 0) {
-                    if (length > 100) length = 100;
-                    bar.fillStyle = "#ffd323";
-                }
-
-                bar.rect(1, 1, 20 * length, 100);
-                bar.fill();
-                // bar.stroke();
-
-                // Nest children in parent node
-                node.appendChild(subNode1);
-                node.appendChild(subNode2);
-            }
-        }
+            div.appendChild(h3);
+            div.appendChild(p);
+            container.appendChild(div);
+        });
     }
-    xmlhttp.open("GET", "php/retrieve_survey_data.php", true);
-    xmlhttp.send();
+}
+request.send();
