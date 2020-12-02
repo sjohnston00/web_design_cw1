4d63df88aedfa1d92ec3bed13f9ce0c4563e17a6
Ross McLean <ross_mclean@posteo.net>
2020-12-02 01:42:31 +0000

    Complete contact form functionality - Send data to database - redirect to confirmation URL and create dynamic user acknowledgement
    Signed-off-by: Ross McLean <ross_mclean@posteo.net>

--- scripts/php/process_contact_data.php
+++ scripts/php/process_contact_data.php
@@ -1,3 +1,9 @@
+<!--
+file: process_contact_data.php
+author: Ross McLean
+desc: Sends the form input data to the database and redirects to the confirmation page
+-->
+
 <?php
 $servername = "localhost";
 $username = "root";
@@ -30,6 +36,13 @@ $message = $_POST['message'];
 $query->execute();
 $conn = null;
 
-echo 'Hi ' . $_POST['forename'] . ' ' . $_POST['surname'] . ' thanks for the following message:<br />';
-echo '<br />' . $_POST['message'] . '<br />';
-echo 'We will contact you at ' . $_POST['email'] . ' soon.<br />';
+// TODO: Sanitise the variables in the URL so they are not visible once sent
+// https://www.plus2net.com/php_tutorial/variables2.php
+redirect("../../form_confirmation.html?fname=$forename&sname=$surname&msg=$message");
+
+// Use to redirect to a specified URL. Since this is a helper function, it might be better off somewhere else.
+function redirect($url, $statusCode = 303)
+{
+    header("Location: " . $url, true, $statusCode);
+    die();
+}
