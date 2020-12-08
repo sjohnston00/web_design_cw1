834ae1b3ce6270c269b058fad832d126b384b01c
Ross McLean <ross_mclean@posteo.net>
2020-12-08 21:31:22 +0000

    Create unique job pages - Get basic data - Create list of tasks
    Signed-off-by: Ross McLean <ross_mclean@posteo.net>

--- scripts/js/job.js
+++ scripts/js/job.js
@@ -4,7 +4,37 @@ author: Ross McLean
 desc: Load unique context per job to the job.html page
  */
 
-// document on ready
-$(function () {
-    document.getElementById("job-title").innerText = "bob";
-})
+const urlQueryString = window.location.search;
+const urlParameters = new URLSearchParams(urlQueryString);
+const soc = urlParameters.get("soc");
+const requestUrl = `http://api.lmiforall.org.uk/api/v1/soc/code/${soc}`;
+
+fetch(requestUrl)
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById("job-title").innerText = data.title;
+        document.getElementById("soc-text").innerText = data.description;
+
+        document.getElementById("qualifications-title").innerText = "Qualifications";
+        document.getElementById("qualifications-paragraph").innerText = data.qualifications;
+
+        document.getElementById("tasks-title").innerText = "Tasks";
+        const tasksList = document.getElementById("tasks-list");
+        let subArray = [];
+        const regex = /^[A-Za-z\s]+$/;  // only match letters and spaces
+        [...data.tasks].forEach(character => {
+            if (character.match(regex)) {
+                subArray.push(character)
+            } else {
+                const listItem = document.createElement("li");
+                listItem.innerHTML = subArray.join("");
+                tasksList.appendChild(listItem);
+                subArray = [];
+            }
+        });
+
+        document.getElementById("tags-title").innerText = "Tags";
+        document.getElementById("tags-paragraph").innerText = `[${data.add_titles.slice(0, 10)}]`;
+
+    })
+    .catch(error => console.log(`response error: ${error}`));
