d077c7e69a32a1573a9a5251b5e9e74a92f49fc7
Ross McLean <ross_mclean@posteo.net>
2020-12-08 21:46:45 +0000

    Update unique job CSS
    Signed-off-by: Ross McLean <ross_mclean@posteo.net>

--- scripts/js/job.js
+++ scripts/js/job.js
@@ -12,23 +12,26 @@ const requestUrl = `http://api.lmiforall.org.uk/api/v1/soc/code/${soc}`;
 fetch(requestUrl)
     .then(response => response.json())
     .then(data => {
-        document.getElementById("job-title").innerText = data.title;
-        document.getElementById("soc-text").innerText = data.description;
+            document.getElementById("job-title").innerText = data.title;
+            document.getElementById("soc-text").innerText = `SOC code: ${data.soc}`;
+            document.getElementById("description-paragraph").innerText = data.description;
 
-        document.getElementById("qualifications-title").innerText = "Qualifications";
-        document.getElementById("qualifications-paragraph").innerText = data.qualifications;
+            document.getElementById("qualifications-title").innerText = "Qualifications";
+            document.getElementById("qualifications-paragraph").innerText = data.qualifications;
 
-        document.getElementById("tasks-title").innerText = "Tasks";
-        const tasksList = document.getElementById("tasks-list");
-        let subArray = [];
-        const regex = /^[A-Za-z\s]+$/;  // only match letters and spaces
-        [...data.tasks].forEach(character => {
-            if (character.match(regex)) {
-                subArray.push(character)
-            } else {
-                const listItem = document.createElement("li");
-                listItem.innerHTML = subArray.join("");
-                tasksList.appendChild(listItem);
+            document.getElementById("tasks-title").innerText = "Tasks";
+
+            // Divide tasks into individual list items
+            const tasksList = document.getElementById("tasks-list");
+            let subArray = [];
+            const regex = /^[A-Za-z\s]+$/;  // only match letters and spaces
+            [...data.tasks].forEach(character => {
+                    if (character.match(regex)) {
+                            subArray.push(character)
+                    } else {
+                            const listItem = document.createElement("li");
+                            listItem.innerHTML = subArray.join("");
+                            tasksList.appendChild(listItem);
                 subArray = [];
             }
         });
