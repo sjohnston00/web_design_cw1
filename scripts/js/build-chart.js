0cfede2d348d84c5cebe9b5d44b11abe95be9249
Ross McLean <ross_mclean@posteo.net>
2020-12-12 17:37:00 +0000

    Complete table
    Signed-off-by: Ross McLean <ross_mclean@posteo.net>

--- scripts/js/build-chart.js
+++ scripts/js/build-chart.js
@@ -8,36 +8,48 @@ export const generateChart = estimatedPayData => {
   const chartYears = []
   const chartWages = []
 
+  estimatedPayData.forEach(object => {
+    chartYears.push(object.year)
+    chartWages.push(object.estpay * 52)  // multiply weekly earnings represent a year
+    console.log(chartYears)
+    console.log(chartWages)
+  })
+
   const ctx = document.getElementById('myChart').getContext('2d')
   const myChart = new Chart(ctx, {
     type: 'line',
     data: {
-      labels: ['2013', '2015', '2017', '2018'],  // loop data
+      labels: chartYears,  // loop data
       datasets: [{
         label: 'Estimated mean yearly pay',
-        data: [100, 500, 600, 1000],
+        data: chartWages,
         backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
+          'rgba(128, 200, 76, 0.2)'
         ],
         borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
+          'rgba(128, 200, 76, 1.0)',
+          'rgba(201, 51, 62, 1.0)',
+          'rgba(32, 50, 139, 1.0)'
         ],
-        borderWidth: 1
+        borderWidth: 2
       }]
     },
     options: {
       scales: {
         yAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: 'Pay (£)'
+          },
+          ticks: {
+            beginAtZero: true
+          }
+        }],
+        xAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: 'Year'
+          },
           ticks: {
             beginAtZero: true
           }
